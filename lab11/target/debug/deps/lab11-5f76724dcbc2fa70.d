d903786457564b7a5afed7bc9ee5b08ed8daa3c3
leah900 <203128980+leah900@users.noreply.github.com>
2025-11-13 10:55:09 -0500

    Ignore lab11 target/ and clean build artifacts from repo

--- lab11/target/debug/deps/lab11-5f76724dcbc2fa70.d
+++
@@ -1,6 +0,0 @@
-/Users/liyatslassie/Documents/GitHub/IS4010-Leah-labs/lab11/target/debug/deps/lab11-5f76724dcbc2fa70.d: src/main.rs src/student.rs
-
-/Users/liyatslassie/Documents/GitHub/IS4010-Leah-labs/lab11/target/debug/deps/lab11-5f76724dcbc2fa70: src/main.rs src/student.rs
-
-src/main.rs:
-src/student.rs:
